a7b940f659d99512540fe3f8fea682bda7cd1a9a
vincenttieu <vtieu@sbcglobal.net>
2022-04-17 23:16:59 -0700

    implement setvel

--- motor.py
+++ motor.py
@@ -71,22 +71,71 @@ class Motor():
       self.io.set_PWM_dutycycle(MTR2_LEGA, 255)
       self.io.set_PWM_dutycycle(MTR2_LEGB, 255+rightdutycycle)
 
-  def setlinear(self, speed):
+  def setlinear(self, speed, run=True, friction_compensation=92):
+    # set run to True to actually set the pwm
+    # if run is false, only return the pwm values
     # speed = 0.31 * (pwm - 92)
-    pwm = speed/0.31 + 92
-    self.set(pwm, pwm)
+    if speed > 0.0:
+      pwm = speed/0.31 + friction_compensation
+    elif speed == 0.0:
+      pwm = 0
+    else:
+      pwm = speed/0.31 - friction_compensation
+      
+    if run:
+      # Safety to ensure pwm values don't exceed 255
+      if pwm > 255 or pwm < -255:
+        print('Calculated PWM ({}) Out of Bounds'.format(pwm))
+        raise KeyboardInterrupt
+      self.set(pwm, pwm)
+    else:
+      return pwm
 
-  def setspin(self, speed):
+  def setspin(self, speed, run=True, friction_compensation=105):
     # speed = 1.26 * (pwm - 105)
-    pwm = speed/1.26 + 105
-    self.set(pwm, -pwm)
+    # Angular velocity in degrees/s
+    # Counterclockwise is positive
+    
+    if speed > 0.0:
+      pwm = speed/1.26 + friction_compensation
+    elif speed == 0.0:
+      pwm = 0
+    else:
+      pwm = speed/1.26 - friction_compensation
+      
+    if run:
+      if pwm > 255 or pwm < -255:
+        print('Calculated PWM ({}) Out of Bounds'.format(pwm))
+        raise KeyboardInterrupt
+      self.set(-pwm, pwm)
+    else:
+      return pwm
     
   def setvel(self, linear, spin):
-    leftspeed = 198/6
-    rightspeed = 116/6
-    leftpwm = leftspeed/0.31 + 92
-    rightpwm = rightspeed/0.31 + 92
+    # Linear portion of setvel
+    pwm_linear = self.setlinear(linear, run=False, friction_compensation=100)
+    print(pwm_linear)
+    # Spin portion of setvel
+    pwm_spin = self.setspin(spin, run=False, friction_compensation=20)
+    print(pwm_spin)
+    # Left and Right PWM
+    leftpwm = pwm_linear - pwm_spin
+    rightpwm = pwm_linear + pwm_spin
+    
+    if (leftpwm > 255 or leftpwm < -255) or (rightpwm > 255 or rightpwm < -255):
+      print('Calculated PWM ({}, {}) Out of Bounds'.format(leftpwm, rightpwm))
+      raise KeyboardInterrupt
+    
     self.set(leftpwm, rightpwm)
+    print(leftpwm)
+    print(rightpwm)
+    
+    # Circle test
+    #leftspeed = 198/6
+    #rightspeed = 116/6
+    #leftpwm = leftspeed/0.31 + 92
+    #rightpwm = rightspeed/0.31 + 92
+    #self.set(leftpwm, rightpwm)
 
 def p5():
   slow = 150
@@ -128,7 +177,7 @@ def p7():
 if __name__ == "__main__":
   motor = Motor()
   try:
-    motor.setvel(0,0)
+    motor.setvel(157/12,360/12)
     time.sleep(12)
     motor.shutdown()  
   except KeyboardInterrupt:
